18d2ca4387268896ea429c818817d8f536ac509d
mitsuhiro <papu.m0917@icloud.com>
2022-01-10 00:01:25 +0900

    TypeScriptを使えるよに修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Header from './components/header'
 
+type Todo = {
+    value: string,
+}
 
 export default function Home() {
+    const [text, setText] = useState('');
+
     return (
         <div>
             <Header title={`Next.js!!!`}></Header>
             <form onSubmit={(e) => e.preventDefault()}>
-                <input type="text" value="" onChange={(e) => e.preventDefault()} />
+                <input
+                    type="text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
                 <input
                     type="submit"
                     value="追加"
